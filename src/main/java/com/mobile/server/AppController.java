2e458c4ac9f1314784804b8fae051a20b92a4764
White_DeepDive <tlgud119@yu.ac.kr>
2025-09-27 21:43:37 +0900

    docs: swagger 의존성 추가 및 간단한 설정

--- src/main/java/com/mobile/server/AppController.java
+++ src/main/java/com/mobile/server/AppController.java
@@ -1,13 +1,16 @@
 package com.mobile.server;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
-    @GetMapping("/")
-    public String home(){
-        return "Hello World!";
+    @GetMapping(value = "/",consumes = "application/json", produces = "application/json")
+    @Operation(summary = "Test API", description = "Returns a simple Hello World message.")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("Hello World");
     }
 }
