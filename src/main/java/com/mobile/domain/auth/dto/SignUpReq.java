f128f9214afe74ebb93778b64493ba62165eed45
silverblond <kdg23538@yu.ac.kr>
2025-10-01 01:04:03 +0900

    [feat/registerReq dto 어노테이션 추가]

--- src/main/java/com/mobile/domain/auth/dto/SignUpReq.java
+++ src/main/java/com/mobile/domain/auth/dto/SignUpReq.java
@@ -2,7 +2,13 @@ package com.mobile.domain.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpReq {
     @NotBlank
     @Size(min = 2, max = 30)
