45badd2ecedfbf12b3a755085636016357466b7a
silverblond <kdg23538@yu.ac.kr>
2025-10-02 14:43:09 +0900

    [feat/signInReqDTO 수정]

--- src/main/java/com/mobile/domain/auth/dto/SignInReq.java
+++ src/main/java/com/mobile/domain/auth/dto/SignInReq.java
@@ -1,13 +1,11 @@
 package com.mobile.domain.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter
 public class SignInReq {
     @NotBlank
     String studentId;
