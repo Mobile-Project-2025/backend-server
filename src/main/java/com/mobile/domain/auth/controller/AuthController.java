eb6596f49e706c3413d600ae72cbb9b5503f7d5e
silverblond <kdg23538@yu.ac.kr>
2025-10-02 15:58:45 +0900

    [feat/로그인 API 구현]

--- src/main/java/com/mobile/domain/auth/controller/AuthController.java
+++ src/main/java/com/mobile/domain/auth/controller/AuthController.java
@@ -1,6 +1,14 @@
 package com.mobile.domain.auth.controller;
 
+import com.mobile.domain.auth.dto.SignInReq;
+import com.mobile.domain.auth.dto.SignInRes;
+import com.mobile.domain.auth.entity.User;
+import com.mobile.domain.auth.jwt.JWTService;
+import com.mobile.domain.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final UserService userService;
+    private final JWTService jwtService;
+
+    //로그인
+    @PostMapping("/login")
+    public ResponseEntity<SignInRes> login(@RequestBody SignInReq signInReq) {
+        User user = userService.verify(signInReq.getStudentId(), signInReq.getPassword());
+        String access = jwtService.generateAccessToken(user);
+        return ResponseEntity.ok(new SignInRes(access, user.getNickname(), user.getRole()));
+    }
 }
