3d61f1592d632fb5b4b70a2f1709dcaccea5fe20
silverblond <kdg23538@yu.ac.kr>
2025-10-02 14:41:25 +0900

    [feat/UserDTO 생성]

--- src/main/java/com/mobile/domain/auth/dto/UserDto.java
+++ src/main/java/com/mobile/domain/auth/dto/UserDto.java
@@ -1,4 +1,12 @@
 package com.mobile.domain.auth.dto;
 
+import com.mobile.domain.auth.entity.RoleType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
+    private Long id;
+    private RoleType role;
 }
