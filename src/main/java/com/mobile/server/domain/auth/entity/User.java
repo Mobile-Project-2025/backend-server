7a6832b8b6683854f3553056fc0692170d0b19ec
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-26 21:54:35 +0900

    feat(User): UserDomain 필드 추가 및 변경
    기본 생성자와 모든 필드를 받는 생성자의 Scope를 제한하여 Builder를 통한 객체 생성을 할 수 있도록 변경

--- src/main/java/com/mobile/server/domain/auth/entity/User.java
+++ src/main/java/com/mobile/server/domain/auth/entity/User.java
@@ -1,12 +1,23 @@
 package com.mobile.server.domain.auth.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Table(name = "users")
 public class User {
@@ -27,4 +38,8 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private RoleType role;
+
+    @Column(nullable = false)
+    private Long cumulativePoint = 0L;
+
 }
