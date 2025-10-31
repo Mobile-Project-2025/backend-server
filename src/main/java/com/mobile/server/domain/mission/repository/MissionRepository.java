c25f2185b90112aa34a88a0d820d812a3a4e8a70
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 23:10:13 +0900

    feat(MissionRespository): 관리자의 마감 미션 조회를 위한 쿼리 호출 함수 작성

--- src/main/java/com/mobile/server/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/mobile/server/domain/mission/repository/MissionRepository.java
@@ -1,7 +1,22 @@
 package com.mobile.server.domain.mission.repository;
 
 import com.mobile.server.domain.mission.domain.Mission;
+import com.mobile.server.domain.missionParticipation.eum.MissionParticipationStatus;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    @Query("""
+                SELECT m
+                FROM MissionParticipation p
+                JOIN p.mission m
+                WHERE m.status = :missionStatus
+                  AND p.participationStatus = :missionParticipationStatus
+            """)
+    List<Mission> findAllByMissionStatusAndMissionParticipationStatus(
+            @Param("missionStatus") com.mobile.server.domain.mission.e.MissionStatus missionStatus,
+            @Param("missionParticipationStatus") MissionParticipationStatus status);
 }
