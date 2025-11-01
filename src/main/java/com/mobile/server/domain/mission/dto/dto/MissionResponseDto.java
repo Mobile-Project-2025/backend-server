e2ec770576d902d008ca64f901babad882d33e40
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-01 14:14:14 +0900

    fix(MissionResponseDto): 생성 날짜 필드 추가
    - 응답 받은 미션을 날짜별로 오름차순 정렬하기 위한 미션 생성 날짜 필드 추가

--- src/main/java/com/mobile/server/domain/mission/dto/dto/MissionResponseDto.java
+++ src/main/java/com/mobile/server/domain/mission/dto/dto/MissionResponseDto.java
@@ -3,8 +3,10 @@ package com.mobile.server.domain.mission.dto.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -43,6 +45,11 @@ public class MissionResponseDto {
     @Schema(description = " 현재까지 참여한 인원 수 (돌발 미션인 경우에만 해당 필드 존재)")
     @PositiveOrZero
     Integer participationCount;
+
+    @Schema(description = "미션 생성 날짜")
+    @NotNull
+    @PastOrPresent
+    LocalDateTime createdAt;
 
 
 }
