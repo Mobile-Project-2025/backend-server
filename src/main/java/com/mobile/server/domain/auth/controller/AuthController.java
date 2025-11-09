d5dec70a2fdf2aab0c616c1c7e99d9b03db6a194
silverblond <kdg23538@yu.ac.kr>
2025-11-09 23:52:41 +0900

    feat(duplicate): 학번 및 닉네임 중복 확인 엔드포인트 추가

--- src/main/java/com/mobile/server/domain/auth/controller/AuthController.java
+++ src/main/java/com/mobile/server/domain/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.mobile.server.domain.auth.controller;
 
+import com.mobile.server.domain.auth.dto.CheckDuplicationRes;
 import com.mobile.server.domain.auth.dto.SignInReq;
 import com.mobile.server.domain.auth.dto.SignInRes;
 import com.mobile.server.domain.auth.dto.SignUpReq;
@@ -9,9 +10,11 @@ import com.mobile.server.domain.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +30,20 @@ public class AuthController {
     public ResponseEntity<Void> register(@RequestBody SignUpReq signInReq) {
         userService.register(signInReq);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    //학번 중복 체크
+    @GetMapping("/check-student-id")
+    public ResponseEntity<CheckDuplicationRes> checkStudentId(@RequestParam String studentId) {
+        userService.checkStudentIdDuplication(studentId);
+        return ResponseEntity.ok(new CheckDuplicationRes(true));
+    }
+
+    //닉네임 중복 체크
+    @GetMapping("/check-nickname")
+    public ResponseEntity<CheckDuplicationRes> checkNickname(@RequestParam String nickname) {
+        userService.checkNicknameDuplication(nickname);
+        return ResponseEntity.ok(new CheckDuplicationRes(true));
     }
 
     //로그인
