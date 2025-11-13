49846a967246ddee5948e6f072b011be5720b8f9
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-13 12:00:43 +0900

    feat(Service): 전체 미션 조회에 대한 비즈니스 로직 구현

--- src/main/java/com/mobile/server/domain/mission/service/MissionManagementService.java
+++ src/main/java/com/mobile/server/domain/mission/service/MissionManagementService.java
@@ -90,6 +90,13 @@ public class MissionManagementService {
         return makeUniqueMissionResponseList(uniquePendingMission);
     }
 
+    public List<MissionResponseDto> getAllMission(Long userId) {
+        isAdmin(userId);
+        List<Mission> allMission = makeAllMission();
+        return makeUniqueMissionResponseList(new HashSet<>(allMission));
+    }
+
+
     public List<CategoryResponseDto> getCategoryNameList(Long userId) {
         isAdmin(userId);
         return Arrays.stream(MissionCategory.values())
@@ -127,6 +134,11 @@ public class MissionManagementService {
         isOpenMission(mission);
         mission.closeMission();
     }
+
+    private List<Mission> makeAllMission() {
+        return missionRepository.findAll();
+    }
+
 
     private void isOpenMission(Mission mission) {
         if (mission.getStatus().equals(MissionStatus.CLOSED)) {
@@ -267,7 +279,4 @@ public class MissionManagementService {
     }
 
 
-    public List<MissionResponseDto> getAllMission(Long aLong) {
-        
-    }
 }
