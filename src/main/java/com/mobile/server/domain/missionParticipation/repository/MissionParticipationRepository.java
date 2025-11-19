c271159be3ac15cd715da873ccd2190192722ac2
silverblond <kdg23538@yu.ac.kr>
2025-11-19 18:17:07 +0900

    feat(missionDetail): 미션 상세 조회 Repository 메서드 작성

--- src/main/java/com/mobile/server/domain/missionParticipation/repository/MissionParticipationRepository.java
+++ src/main/java/com/mobile/server/domain/missionParticipation/repository/MissionParticipationRepository.java
@@ -1,11 +1,15 @@
 package com.mobile.server.domain.missionParticipation.repository;
 
+import com.mobile.server.domain.auth.entity.User;
+import com.mobile.server.domain.mission.domain.Mission;
 import com.mobile.server.domain.missionParticipation.domain.MissionParticipation;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MissionParticipationRepository extends JpaRepository<MissionParticipation, Long> {
 
+    Optional<MissionParticipation> findByMissionAndUser(Mission mission, User user);
 
 }
